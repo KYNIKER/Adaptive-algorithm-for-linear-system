09d72786d1366b159ec166cb45d1a665bbe8e903
KYNIKER <dhha19.student.aau@gmail.com>
2025-09-23 09:07:11 +0200

    Fix: commited the wrong file lol

--- tightTest.jl
+++ tightTest.jl
@@ -1,25 +1,37 @@
 # Based on the paper JuliaReach: a Toolbox for Set-Based Reachability
 using Plots, LazySets, LinearAlgebra
-tΔ = 0.02
+include("helperfunctions.jl")
 
-function rectangleFromHBox(corners, offset)
-    Shape((getindex.(corners, 1)).+(offset*tΔ), getindex.(corners, 2))
-end
+tΔ = 0.4
+
+
+
+r = 1.5
 
 T = 4
-N = floor(Int, T/tΔ )
+N = floor(Int, T/tΔ)
 μ = 0.001
-A = [-1. 0.; 0. -1.]#UniformScaling(-1.0)
-P₁ = Zonotope([0., 1.5], [0.0 0.0; 0.0 0.5])
+A = [cos(r) sin(r); -sin(r) cos(r)]
+#A = [-1. 0.; 0. -1.]#reshape([-1.0], 1, 1)#UniformScaling(-1.0)#implement with double and add
+P₁ = Zonotope([0., 1.5], [[0.0; 0.05]])#[0.0 0.0; 0.0 0.5])
+#P₁ = Zonotope([0., 2], [0.0 0.0; 0.0 1])
 ANorm = norm(A, Inf)
 α = (exp(ANorm*tΔ)-1-tΔ*ANorm)/norm(P₁, Inf)
 β = (exp(ANorm*tΔ)-1)*μ/ANorm
 
 ϕ = exp(A*tΔ)
+println(dim(P₁))
+
+#=As = Matrix(undef, ceil(Int, log2(N)), 1)
+As[1] = ϕ
+
+for j in 2:size(As, 1)
+    As[j] = As[j-1]*As[j-1]
+end=#
 
 ϕp = (I+ϕ)/2
 ϕm = (I-ϕ)/2
-gens = hcat(ϕp*P₁.generators, ϕm*P₁.center, ϕm*P₁.generators)
+gens = hcat(ϕp*P₁.generators,ϕm*P₁.center, ϕm*P₁.generators )
 
 
 R₁ = minkowski_sum(Zonotope(ϕp*P₁.center, gens), Zonotope(zeros(2), (α+β)*I(2)))
@@ -35,19 +47,17 @@ for i in 2:N
     global R = R∪box_approximation(boxes[i])
 end
 xs = range(0, T, length=N)
-maxes = []
-mines = []
 box = []
 
-plot(size=(500,400), dpi=300, thickness_scaling=1)
+p = plot(dpi=300, thickness_scaling=1)
 
 for i in 1:(N)
     approxBox = box_approximation(boxes[i])
     corners = vertices_list(boxes[i])
-    plot!(rectangleFromHBox(corners, i - 1), c=:blue,lab="")
-    push!(maxes, norm(approxBox, Inf))
-    push!(mines, low(approxBox, 2))
+    plot!(p, rectangleFromHBox(corners), vars=(0,1), c=:blue,lab="")
+    #plot!(p, Shape(getindex.(corners, 1), getindex.(corners, 2)), vars=(0, 1), c=:blue,lab="")
 end
 
-plot!(xs, 1.0 * exp.(-xs), vars=(0, 1), c=:magenta, lab="")
-plot!(xs, 2.0 * exp.(-xs), vars=(0, 1), c=:magenta, lab="")
+plot(p)
+
+#savefig("boxplot_-1_0_0_-1.png")
