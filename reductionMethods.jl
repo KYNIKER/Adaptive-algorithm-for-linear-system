54c84277bd28b694bb568cc176bc14ec6c88e128
Grace Melchiors <melchiorsgrace@gmail.com>
2025-10-17 09:40:34 +0200

    bugfix box_reduce

--- reductionMethods.jl
+++ reductionMethods.jl
@@ -17,14 +17,34 @@ function PCA_reduce(Z, k) # Reduce to k generators
 end
 
 function box_reduce(Z, k) # Reduce to k generators
+
+    if k == 1 # If we only want one generator
+        hyperRect = interval_hull(Z)
+        return Zonotope(Z.center, [radius_hyperrectangle(hyperRect)])
+    end
+
     c = Z.center # get center
     G = genmat(Z) # Get generators
 
+    amountOfGens = size(G, 2)
+    k = min(k, amountOfGens) # Ensure k is not greater than the amount of generators
+
+
     G = sortslices(G, dims = 2, by = x->norm(x, 1) - norm(x, Inf))
 
-    k = min(k, size(G, 2)) # Ensure k is not greater than the amount of generators
 
-    G_sorted = G[:, 1:k]
+    # We split the generators into two groups. Where G1 will be overapproximated
+    G1 = G[:, 1:amountOfGens-(k-1)]
+    G2 = G[:, amountOfGens-(k-2):amountOfGens]
 
-    return Zonotope(c, G_sorted)
+    Z1 = Zonotope(zeros(dim(Z)), G1)
+    Z2 = Zonotope(c, G2)
+
+    Z1HyperRect = interval_hull(Z1) # Note that Z1 will always return something with dims 2. 
+    Z1red = Zonotope(zeros(dim(Z)), [radius_hyperrectangle(Z1HyperRect)])
+
+    Zred = minkowski_sum(Z1red, Z2)
+
+    return Zred
+
 end
