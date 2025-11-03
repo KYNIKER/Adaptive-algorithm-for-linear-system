f3b8fecd44b9087d7b10bfdef7bccf571d5688bd
Grace Melchiors <melchiorsgrace@gmail.com>
2025-11-03 10:19:48 +0100

    cleanup + pseudoMerge

--- reductionMethods.jl
+++ reductionMethods.jl
@@ -1,19 +1,14 @@
 using LazySets, LinearAlgebra
 
-function PCA_reduce(Z, k) # Reduce to k generators
-
-    c = Z.center # get center
+function PCA_reduce(Z) # Reduce to dim(Z) generators
     G = genmat(Z) # Get generators
 
     U, S, V = svd(G) # Apply singular value decomposition (https://www.geeksforgeeks.org/machine-learning/singular-value-decomposition-svd/)
 
-    # Note that U finds the eigenvectors of the covariance matrix
-    # S ranks importance, and is ordered equivalently to eigenvalues
-    k = min(k, length(S))   # Ensure k is not greater than the amount of dimensions
-
-    G_red = U[:, 1:k] * Diagonal(S[1:k]) # Get top k
-
-    return Zonotope(c, G_red)
+    # U finds the eigenvectors of the covariance matrix
+    Z_red = linear_map(U, interval_hull(transpose(U) * Z))
+   
+    return Z_red
 end
 
 function box_reduce(Z, k) # Reduce to k generators
