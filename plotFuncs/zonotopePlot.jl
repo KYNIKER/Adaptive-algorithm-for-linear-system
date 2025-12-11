a59d03640cd2d8580266e49d4511115dc6ef9819
Grace Melchiors <melchiorsgrace@gmail.com>
2025-12-11 10:28:45 +0100

    Change to not use Latex

--- plotFuncs/zonotopePlot.jl
+++ plotFuncs/zonotopePlot.jl
@@ -1,6 +1,5 @@
 # Originally made by Christian Schilling
-
-using LazySets, Plots, Plots.PlotMeasures, LaTeXStrings
+using LazySets, Plots, Plots.PlotMeasures
 
 c = [2, 1.5]
 g1 = [-0.5, 0.3]
@@ -11,30 +10,60 @@ G = hcat(g1, g2, g3, g4)
 Z = Zonotope(c, G)
 println("\nzonotope: c = $c;   G = $G")
 
-fig = plot(xlims=(0, 4), ylims=(-0.3, 3), ratio=1, xlab=L"x_1", ylab=L"x_2",
-           legendfontsize=25,
-           tickfont=font(25, "Times"),
-           guidefontsize=25,
-           xguidefont=font(25, "Times"),
-           yguidefont=font(25, "Times"),
-           xtick = ([0, 1, 2, 3, 4], [L"0", L"1", L"2", L"3", L"4"]),
-           ytick = ([0, 1, 2, 3], [L"0", L"1", L"2", L"3"]),
-           bottom_margin=0mm, left_margin=0mm, right_margin=0mm, top_margin=0mm, size=(500, 450))
+fig = plot(
+    xlims = (0, 4),
+    ylims = (-0.3, 3),
+    ratio = 1,
+    xlab = "X",
+    ylab = "Y",
+    legendfontsize = 25,
+    tickfont = font(25, "Times"),
+    guidefontsize = 25,
+    xguidefont = font(25, "Times"),
+    yguidefont = font(25, "Times"),
+    xtick = ([0, 1, 2, 3, 4], ["0", "1", "2", "3", "4"]),
+    ytick = ([0, 1, 2, 3], ["0", "1", "2", "3"]),
+    bottom_margin = 0mm,
+    left_margin = 0mm,
+    right_margin = 0mm,
+    top_margin = 0mm,
+    size = (500, 450)
+)
 
 plot!(fig, Z)
 
-plot!(fig, [c[1], c[1] + g1[1]], [c[2], c[2] + g1[2]], linecolor=:red, arrow=:arrow,
-      linestyle=:dot, width=2, annotations=(1.4, 2.0, text(L"g_1", 20)), lab="")
+plot!(
+    fig, [c[1], c[1] + g1[1]], [c[2], c[2] + g1[2]],
+    linecolor = :red, arrow = :arrow, linestyle = :dot, width = 2,
+    annotations = (1.4, 2.0, text("g₁", 20)),
+    lab = ""
+)
 
-plot!(fig, [c[1], c[1] + g2[1]], [c[2], c[2] + g2[2]], linecolor=:red, arrow=:arrow,
-      linestyle=:dot, width=2, annotations=(2.5, 2.5, text(L"g_2", 20)), lab="")
+plot!(
+    fig, [c[1], c[1] + g2[1]], [c[2], c[2] + g2[2]],
+    linecolor = :red, arrow = :arrow, linestyle = :dot, width = 2,
+    annotations = (2.5, 2.5, text("g₂", 20)),
+    lab = ""
+)
 
-plot!(fig, [c[1], c[1] + g3[1]], [c[2], c[2] + g3[2]], linecolor=:red, arrow=:arrow,
-      linestyle=:dot, width=2, annotations=(2.5, 1.5, text(L"g_3", 20)), lab="")
+plot!(
+    fig, [c[1], c[1] + g3[1]], [c[2], c[2] + g3[2]],
+    linecolor = :red, arrow = :arrow, linestyle = :dot, width = 2,
+    annotations = (2.5, 1.5, text("g₃", 20)),
+    lab = ""
+)
 
-plot!(fig, [c[1], c[1] + g4[1]], [c[2], c[2] + g4[2]], linecolor=:red, arrow=:arrow,
-      linestyle=:dot, width=2, annotations=(2.0, 2.0, text(L"g_4", 20)), lab="")
+plot!(
+    fig, [c[1], c[1] + g4[1]], [c[2], c[2] + g4[2]],
+    linecolor = :red, arrow = :arrow, linestyle = :dot, width = 2,
+    annotations = (2.0, 2.0, text("g₄", 20)),
+    lab = ""
+)
 
-plot!(fig, Singleton(c), annotations=(2, 1.3, text("c", 20)), color=:red, alpha=1.)
+plot!(
+    fig, Singleton(c),
+    annotations = (2, 1.3, text("c", 20)),
+    color = :red, alpha = 1.
+)
 
 savefig(fig, "zonotope.pdf")
