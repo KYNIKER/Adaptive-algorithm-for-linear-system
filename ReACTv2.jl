9582680c5d5c0e6fa8cf449226d8c822ede53141
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2026-02-09 14:14:38 +0100

    Feat: Reorganized the ReACT function + reverted from FastExpm to Base.exp() + Implemented ReACT with support functions utilizing the transposed flow matrix

---
+++ ReACTv2.jl
@@ -0,0 +1,170 @@
+using LazySets, LinearAlgebra
+
+function ReACTWithSupport(A, B, initialTimeStep, interval, X0::Zonotope{N,Vector{N},Matrix{N}}, U::Zonotope, constraint, Digits::Integer, STRATEGY::Integer) where {N}
+    XDim, _ = size(genmat(X0))
+    m = initialTimeStep / 2^(ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)   #Calculate the smallest number larger than 10^-Digits obtained by repeatedly dividing initialTimeStep by 2.
+    changedTimeStep = true
+    elems = (ceil(Integer, log2(initialTimeStep)) + ceil(Integer, -log2(10.0^(-Digits))) - 1)
+    phiDict = Dict{Float64,Matrix{Float64}}()
+    sizehint!(phiDict, elems)
+    discritezationDict = Dict{Float64,Zonotope{N,Vector{N},Matrix{N}}}()
+    sizehint!(discritezationDict, elems)
+    inputDiscritezationDict = Dict{Float64,Zonotope{N,Vector{N},Matrix{N}}}()
+    sizehint!(inputDiscritezationDict, elems)
+
+    constraintProjVectors = map(x -> x.a, constraint)
+    constraintProjBounds = ρ.(constraintProjVectors, constraint)
+
+
+
+    time::Float64 = minimum(interval)
+    endtime::Float64 = maximum(interval)
+
+    currentTimeStep = copy(initialTimeStep)
+
+    attemptsRecorder = Integer[]
+
+    U = concretize(B * U)
+
+    let ϕ::Matrix{Float64} = exp(A .* m)
+        tempM = similar(ϕ)
+        d = m
+        dia::Matrix{Float64} = diagm(ones(XDim))
+
+        if !(zeros(XDim) ∈ U) #Origin is *not* in input
+            û = copy(U.center)
+            invA = inv(Matrix(A))
+            Ut = Zonotope(U.center - û, genmat(U))
+            dU = overapproximate(d * Ut, Zonotope)
+            P = minkowski_sum(dU, E_ψ(Ut, d, A))
+
+
+            P̂ = invA * (ϕ - dia) * û
+            lt = minkowski_sum(convert(Zonotope, ϕ * X0), dU)
+            rt = minkowski_sum(E_ψ(Ut, d, A), E⁺(X0, d, A))
+            PZ = Zonotope(P̂, zeros(Float64, size(U.center, 1), 1))
+            f = minkowski_sum(lt, rt)
+            disc = overapproximate(CH(X0, minkowski_sum(f, PZ)), Zonotope)
+            lt = missing
+            rt = missing
+            PZ = missing
+            f = missing
+
+            while d < initialTimeStep
+                inputDiscritezationDict[d] = P
+                P = minkowski_sum(P, linear_map(ϕ, P))
+                discritezationDict[d] = disc
+                phiDict[d] = permutedims(copy(ϕ))
+                disc = overapproximate(CH(disc, linear_map(ϕ, disc)), Zonotope)
+
+                mul!(tempM, ϕ, ϕ)
+                copy!(ϕ, tempM)
+                d = d * 2
+            end
+
+            discritezationDict[d] = copy(disc)
+            phiDict[d] = copy(ϕ)
+            inputDiscritezationDict[initialTimeStep] = P
+        else
+            dU = overapproximate(d * U, Zonotope)
+            P = minkowski_sum(dU, E_ψ(U, d, A))
+            lt = concretize(minkowski_sum(convert(Zonotope, ϕ * X0), dU))
+            rt = concretize(minkowski_sum(E_ψ(U, d, A), E⁺(X0, d, A)))
+            f = concretize(minkowski_sum(lt, rt))
+            disc = overapproximate(CH(X0, f), Zonotope)
+            while d < initialTimeStep
+                inputDiscritezationDict[d] = P
+                P = minkowski_sum(P, linear_map(ϕ, P))
+
+
+                phiDict[d] = permutedims(copy(ϕ))
+                discritezationDict[d] = copy(disc)
+                disc = overapproximate(CH(disc, linear_map(ϕ, disc)), Zonotope)
+                mul!(tempM, ϕ, ϕ)
+                copy!(ϕ, tempM)
+                d = d * 2
+            end
+            discritezationDict[d] = copy(disc)
+            phiDict[d] = copy(ϕ)
+            inputDiscritezationDict[initialTimeStep] = P
+        end
+    end
+
+
+
+    V::Zonotope{N,Vector{N},Matrix{N}} = copy(inputDiscritezationDict[initialTimeStep])
+    Sρ = zeros(Float64, length(constraint))
+    newR::Zonotope{N,Vector{N},Matrix{N}} = discritezationDict[initialTimeStep]
+    i = 1
+
+
+    Φ::Matrix{Float64} = diagm(ones(Float64, size(A, 2)))
+    tempM = similar(Φ)
+    ϕt = similar(Φ)
+    newRR = copy(newR)
+
+
+    while time < endtime
+
+        attempts = 1
+        approveFlag = false
+
+        while !approveFlag
+            if currentTimeStep < m
+                return false
+            end
+
+            if changedTimeStep
+                newRR = discritezationDict[currentTimeStep]
+                V = copy(inputDiscritezationDict[currentTimeStep])
+                ϕt = phiDict[currentTimeStep]
+            end
+            constraintProjVectors = map(x -> ϕt * x, constraintProjVectors)
+
+            changedTimeStep = false
+            hom = map(x -> ρ(x, newRR), constraintProjVectors)
+            inhom = map(x -> ρ(x, V), constraintProjVectors)
+
+            if reduce(&, <=(Sρ + hom + inhom, constraintProjBounds))
+                approveFlag = true
+                Sρ += map(x -> ρ(x, V), constraintProjVectors) + map(x -> dot(V.center, x), constraintProjVectors)
+                mul!(tempM, Φ, ϕt)
+                copy!(Φ, tempM)
+            else
+                newR = copy(newR)
+                currentTimeStep = currentTimeStep / 2
+                changedTimeStep = true
+                attempts = attempts + 1
+            end
+        end
+
+
+        push!(attemptsRecorder, attempts)
+        i = i + 1
+        time = time + currentTimeStep
+
+        # Reset / apply strategy
+        # Only do this if the current timestep is less than the initial
+        if STRATEGY == 0
+            # Only reduce
+        elseif STRATEGY == 1
+            # always try double
+            if currentTimeStep < initialTimeStep
+                currentTimeStep = currentTimeStep * 2
+                changedTimeStep = true
+            end
+        elseif STRATEGY == 2
+            # If attemptsrecorder past 4 are successes, double timestep
+            if currentTimeStep < initialTimeStep
+                lowest = min(4, i - 1)
+                window = @view attemptsRecorder[i-lowest:i-1]
+                if all(window .== 1)
+                    currentTimeStep = currentTimeStep * 2
+                    changedTimeStep = true
+                end
+            end
+        end
+    end
+
+    return true
+end
--- helperfunctions.jl
+++ helperfunctions.jl
@@ -3,7 +3,7 @@ using LazySets, LinearAlgebra, Plots, FastExpm
 using Random # For strategy 4
 
 function rectangleFromHBox(res::AbstractVector{Shape}, corners)
-    for i in 1:size(corners,1)
+    for i in 1:size(corners, 1)
         tope = getindex(corners, i)
         res[i] = Shape(getindex.(tope, 1), getindex.(tope, 2))
     end
@@ -21,7 +21,7 @@ function rectangleFromHBox!(res::AbstractVector{Shape}, cornerss, timestepsize, 
         res[i] = Shape([currentTime, currentTime + deltt, currentTime + deltt, currentTime], [mincor, mincor, maxcor, maxcor])
         #res[i] = Shape([deltt*(i-1), deltt*i, deltt*i, deltt*(i-1)], [mincor, mincor, maxcor, maxcor])
         currentTime = currentTime + deltt
-        deltt = deltt+timescale*timestepsize
+        deltt = deltt + timescale * timestepsize
     end
     return res
 end
@@ -61,42 +61,42 @@ function rectangleFromHBox!(res::AbstractVector{Shape}, cornerss, timestepsize, 
         maxcor = maximum(dimCoords)
         mincor = minimum(dimCoords)
         curtime = timesteps[i]
-        res[i] = Shape([timestepsize*(curtime-1), timestepsize*curtime, timestepsize*curtime, timestepsize*(curtime-1)], [mincor, mincor, maxcor, maxcor])
+        res[i] = Shape([timestepsize * (curtime - 1), timestepsize * curtime, timestepsize * curtime, timestepsize * (curtime - 1)], [mincor, mincor, maxcor, maxcor])
     end
     return res
 end
 
 function reachsets(A, timestepsize, interval, X₀, μ, timescale)
     T = maximum(interval)
-    N = floor(Int, T/timestepsize)
-    
+    N = floor(Int, T / timestepsize)
+
     ANorm = norm(A, Inf)
-    α = (exp(ANorm*timestepsize)-1-timestepsize*ANorm)/norm(X₀, Inf)
-    β = (exp(ANorm*timestepsize)-1)*μ/ANorm
+    α = (exp(ANorm * timestepsize) - 1 - timestepsize * ANorm) / norm(X₀, Inf)
+    β = (exp(ANorm * timestepsize) - 1) * μ / ANorm
 
-    ϕ = exp(A*timestepsize)
+    ϕ = exp(A * timestepsize)
 
-    ϕp = (I+ϕ)/2
-    ϕm = (I-ϕ)/2
-    gens = hcat(ϕp*X₀.generators,ϕm*X₀.center, ϕm*X₀.generators)
+    ϕp = (I + ϕ) / 2
+    ϕm = (I - ϕ) / 2
+    gens = hcat(ϕp * X₀.generators, ϕm * X₀.center, ϕm * X₀.generators)
 
-    R₁ = minkowski_sum(Zonotope(ϕp*X₀.center, gens), Zonotope(zeros(2), (α+β)*I(2)))
+    R₁ = minkowski_sum(Zonotope(ϕp * X₀.center, gens), Zonotope(zeros(2), (α + β) * I(2)))
     #res = [R₁]
     R = [R₁]
 
-    ballβ = Zonotope(zeros(2), β*I(2))
+    ballβ = Zonotope(zeros(2), β * I(2))
     i = 2
-    deltt=timestepsize
+    deltt = timestepsize
     time = timestepsize # We have already done one timestep
     #while i < N
     while time < T
-        deltt = deltt+timescale*timestepsize
+        deltt = deltt + timescale * timestepsize
         #α = (exp(ANorm*timestepsize)-1-timestepsize*ANorm)/norm(X₀, Inf)
-        β = (exp(ANorm*deltt)-1)*μ/ANorm
+        β = (exp(ANorm * deltt) - 1) * μ / ANorm
 
-        ϕ = exp(A*deltt)
+        ϕ = exp(A * deltt)
 
-        ballβ = Zonotope(zeros(dim(X₀)), β*I(2))
+        ballβ = Zonotope(zeros(dim(X₀)), β * I(2))
         push!(R, minkowski_sum(linear_map(ϕ, R[i-1]), ballβ))
         #res[i] = R[i]
 
@@ -109,29 +109,29 @@ function reachsets(A, timestepsize, interval, X₀, μ, timescale)
     return R
 end
 
-function initialStep(A::Matrix{Float64}, ANorm::Float64, timestep::Float64, X₀::Zonotope{Float64, Vector{Float64}, Matrix{Float64}}, μ::Float64)
-    
+function initialStep(A::Matrix{Float64}, ANorm::Float64, timestep::Float64, X₀::Zonotope{Float64,Vector{Float64},Matrix{Float64}}, μ::Float64)
+
     # Splitting this up so the compiler doesn't get confused
     step1::Float64 = exp(ANorm * timestep)::Float64 - 1 - timestep * ANorm
     step2::Float64 = manualNormInf(X₀)
     α::Float64 = step1 / step2
     #α = (exp(ANorm*timestep)-1-timestep*ANorm)/norm(X₀, Inf)
-    β::Float64 = (exp(ANorm*timestep)::Float64-1)*μ/ANorm
+    β::Float64 = (exp(ANorm * timestep)::Float64 - 1) * μ / ANorm
 
-    ϕ = exp(A*timestep)::Matrix{Float64}
+    ϕ = exp(A * timestep)::Matrix{Float64}
 
-    ϕp::Matrix{Float64} = (I+ϕ)/2
-    ϕm::Matrix{Float64} = (I-ϕ)/2
-    gens = hcat(ϕp*X₀.generators,ϕm*X₀.center, ϕm*X₀.generators)::Matrix{Float64}
+    ϕp::Matrix{Float64} = (I + ϕ) / 2
+    ϕm::Matrix{Float64} = (I - ϕ) / 2
+    gens = hcat(ϕp * X₀.generators, ϕm * X₀.center, ϕm * X₀.generators)::Matrix{Float64}
 
-    R::Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = minkowski_sum(Zonotope(ϕp*X₀.center, gens), Zonotope(zeros(dim(X₀)), (α+β)*I(dim(X₀))))
+    R::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = minkowski_sum(Zonotope(ϕp * X₀.center, gens), Zonotope(zeros(dim(X₀)), (α + β) * I(dim(X₀))))
 
-    ballβ = Zonotope(zeros(dim(X₀)), diagm(β*ones(dim(X₀))))
+    ballβ = Zonotope(zeros(dim(X₀)), diagm(β * ones(dim(X₀))))
 
     return (R, ballβ, ϕ)
 end
 
-function manualNormInf(Z::Zonotope{Float64, Vector{Float64}, Matrix{Float64}})
+function manualNormInf(Z::Zonotope{Float64,Vector{Float64},Matrix{Float64}})
     dimensions = dim(Z)::Int64
     maxVal::Float64 = Z.center[1] + sum(Z.generators[1])
     for i in 2:dimensions
@@ -142,23 +142,23 @@ function manualNormInf(Z::Zonotope{Float64, Vector{Float64}, Matrix{Float64}})
 end
 
 
-function initialStepNoInput(A::Matrix{Float64}, Anorm::Float64, timestep::Float64, X₀::Zonotope{Float64, Vector{Float64}, Matrix{Float64}})
-    
+function initialStepNoInput(A::Matrix{Float64}, Anorm::Float64, timestep::Float64, X₀::Zonotope{Float64,Vector{Float64},Matrix{Float64}})
+
     # Splitting this up so the compiler doesn't get confused
     step1 = exp(Anorm * timestep)::Float64 - 1 - timestep * Anorm
     step2::Float64 = manualNormInf(X₀)
     α = step1 / step2
-    
+
     #α::Float64 = (exp(ANorm*timestep)-1-timestep*ANorm)/manualNormInf(X₀)::Float64
 
-    ϕ = exp(A*timestep)::Matrix{Float64}
+    ϕ = exp(A * timestep)::Matrix{Float64}
 
-    ϕp::Matrix{Float64} = (I+ϕ)/2
-    ϕm::Matrix{Float64} = (I-ϕ)/2
+    ϕp::Matrix{Float64} = (I + ϕ) / 2
+    ϕm::Matrix{Float64} = (I - ϕ) / 2
 
-    gens = hcat(ϕp*X₀.generators,ϕm*X₀.center, ϕm*X₀.generators)::Matrix{Float64}
+    gens = hcat(ϕp * X₀.generators, ϕm * X₀.center, ϕm * X₀.generators)::Matrix{Float64}
 
-    R::Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = minkowski_sum(Zonotope(ϕp*X₀.center, gens), Zonotope(zeros(dim(X₀)), α*I(dim(X₀))))
+    R::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = minkowski_sum(Zonotope(ϕp * X₀.center, gens), Zonotope(zeros(dim(X₀)), α * I(dim(X₀))))
 
     return R, ϕ
 end
@@ -171,7 +171,7 @@ function reachSetsForTimesteps(A, timesteps, interval, X₀, μ)
 
     R₁, ballβ, ϕ = initialStep(A, ANorm, timestep, X₀, μ)
 
-    
+
     R = [R₁]
 
     # We only further utilize these if we have an input
@@ -204,9 +204,9 @@ function reachSetsForTimesteps(A, timesteps, interval, X₀, μ)
 
         else # Recalculate values for new timestep
             newR, ballβ, ϕ = initialStep(A, ANorm, timestep, X₀, μ)
-            
+
             # We move R to the current time
-            ϕCurrentTime = exp(A*currentTime)
+            ϕCurrentTime = exp(A * currentTime)
             newR = Zonotope(ϕCurrentTime * newR.center, ϕCurrentTime * newR.generators)
 
             push!(R, newR)
@@ -220,7 +220,7 @@ function reachSetsForTimesteps(A, timesteps, interval, X₀, μ)
                     S = minkowski_sum(S, V)
                     V = linear_map(ϕ, V)
                 end
-                
+
                 push!(Ω, minkowski_sum(newR, S))
             end
         end
@@ -235,16 +235,16 @@ function reachSetsForTimesteps(A, timesteps, interval, X₀, μ)
 end
 
 # Move a zonotope forward in time, assumes no input
-function forwardTimeNoInput(A, R, currentTime) 
-    ϕCurrentTime = exp(A*currentTime)
+function forwardTimeNoInput(A, R, currentTime)
+    ϕCurrentTime = exp(A * currentTime)
     return linear_map(ϕCurrentTime, R)
 end
 
 # We reuse the previous S, and do not recompute it
-function forwardTimeReUse(A, R, currentTime, ϕ, ballβ, prevS) 
-    newR = linear_map(exp(A*currentTime), R)
+function forwardTimeReUse(A, R, currentTime, ϕ, ballβ, prevS)
+    newR = linear_map(exp(A * currentTime), R)
     #newR = forwardTimeNoInput(A, R, currentTime)
-    V = linear_map(exp(A*currentTime), ballβ)
+    V = linear_map(exp(A * currentTime), ballβ)
 
     S = minkowski_sum(prevS, V)
     V = linear_map(ϕ, V)
@@ -255,17 +255,17 @@ function forwardTimeReUse(A, R, currentTime, ϕ, ballβ, prevS)
     return (newR, S, V, newΩ)
 end
 
-function forwardTime(A :: Matrix{Float64}, R :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}}, currentTime :: Float64, timestep :: Float64, ϕ :: Matrix{Float64}, prevTime :: Float64, prevS :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}}, prevV :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}})
-    newR :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = linear_map(exp(A*currentTime), R)
+function forwardTime(A::Matrix{Float64}, R::Zonotope{Float64,Vector{Float64},Matrix{Float64}}, currentTime::Float64, timestep::Float64, ϕ::Matrix{Float64}, prevTime::Float64, prevS::Zonotope{Float64,Vector{Float64},Matrix{Float64}}, prevV::Zonotope{Float64,Vector{Float64},Matrix{Float64}})
+    newR::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = linear_map(exp(A * currentTime), R)
     #newR :: Zonotope = forwardTimeNoInput(A, R, currentTime)
     steps = floor(Int, (currentTime - prevTime) / timestep)
-    S = prevS 
-    V = prevV 
+    S = prevS
+    V = prevV
     for j in 1:steps
-        S :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = minkowski_sum(S, V) # this one
-        V :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = linear_map(ϕ, V) # this one
+        S::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = minkowski_sum(S, V) # this one
+        V::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = linear_map(ϕ, V) # this one
     end
-    newΩ :: Zonotope{Float64, Vector{Float64}, Matrix{Float64}} = minkowski_sum(newR, S)
+    newΩ::Zonotope{Float64,Vector{Float64},Matrix{Float64}} = minkowski_sum(newR, S)
 
     #println("Set S, V: $S, $V with steps $steps")
     return (newR, S, V, newΩ)
@@ -273,8 +273,8 @@ end
 
 function reachsetslog(A, timestepsize, interval, X₀, μ)
     T = maximum(interval)
-    N = floor(Int, T/timestepsize)
-    
+    N = floor(Int, T / timestepsize)
+
     bits = ceil(Int, log2(N))
     println(N)
     println(bits)
@@ -285,36 +285,36 @@ function reachsetslog(A, timestepsize, interval, X₀, μ)
 
 
     ANorm = norm(A, Inf)
-    α = (exp(ANorm*timestepsize)-1-timestepsize*ANorm)/norm(X₀, Inf)
-    β = (exp(ANorm*timestepsize)-1)*μ/ANorm
+    α = (exp(ANorm * timestepsize) - 1 - timestepsize * ANorm) / norm(X₀, Inf)
+    β = (exp(ANorm * timestepsize) - 1) * μ / ANorm
 
-    ϕ = exp(A*timestepsize)
+    ϕ = exp(A * timestepsize)
 
-    ϕp = (I+ϕ)/2
-    ϕm = (I-ϕ)/2
-    gens = hcat(ϕp*X₀.generators,ϕm*X₀.center, ϕm*X₀.generators)
+    ϕp = (I + ϕ) / 2
+    ϕm = (I - ϕ) / 2
+    gens = hcat(ϕp * X₀.generators, ϕm * X₀.center, ϕm * X₀.generators)
 
-    R₁ = minkowski_sum(Zonotope(ϕp*X₀.center, gens), Zonotope(zeros(2), (α+β)*I(2)))
+    R₁ = minkowski_sum(Zonotope(ϕp * X₀.center, gens), Zonotope(zeros(2), (α + β) * I(2)))
     #res = [R₁]
     R = [R₁]
     ts = [1]
 
-    ballβ = Zonotope(zeros(2), β*I(2))
+    ballβ = Zonotope(zeros(2), β * I(2))
 
     ϕs[1] = ϕ
     ballβs[1] = ballβ
 
     for i in 2:bits
-        ϕs[i] = ϕs[i-1]*ϕs[i-1]
+        ϕs[i] = ϕs[i-1] * ϕs[i-1]
         ballβs[i] = minkowski_sum(ballβs[i-1], ballβs[i-1])
     end
 
     for i in 2:bits
         if ((N >> (i)) & 1) == 1
             push!(R, minkowski_sum(linear_map(ϕs[i], R[end]), ballβs[i]))
-            push!(ts, ts[end]+(2^(i)))
+            push!(ts, ts[end] + (2^(i)))
         end
-            #res[i] = R[i]
+        #res[i] = R[i]
     end
     return R, ts
 end
@@ -323,7 +323,7 @@ function approximateArea(zonotope::Zonotope)
     zonotope |> box_approximation |> area
 end
 
-function approximateArea(zonotope::AbstractVector{Zonotope{Float64, Vector{Float64}, Matrix{Float64}}})
+function approximateArea(zonotope::AbstractVector{Zonotope{Float64,Vector{Float64},Matrix{Float64}}})
     area(reduce(∪, map(box_approximation, zonotope)))
 end
 
@@ -333,7 +333,7 @@ end
 
 #intersects function is based upon "Set operations and order reductions for constrained zonotopes" - Vignesh Raghuraman, Justin P. Koeln
 #Note that this only returns true if they intersect and does not depend on whether the intersection between them is empty.
-function intersectss(c::Vector{Float64}, G::Matrix{Float64}, h::Vector{Float64}, f::Float64, tempM :: Matrix{Float64}) :: Bool #Kan måske speedes op ved at give et ekstra argument tempM som bliver brugt til at udregne højresiden så at vi ikke altid assigner memory til den operation.
+function intersectss(c::Vector{Float64}, G::Matrix{Float64}, h::Vector{Float64}, f::Float64, tempM::Matrix{Float64})::Bool #Kan måske speedes op ved at give et ekstra argument tempM som bliver brugt til at udregne højresiden så at vi ikke altid assigner memory til den operation.
     #c::Vector{Float64} = zonotope.center
     #G::Matrix{Float64} = genmat(zonotope)
     #println(h, reshape(halfspace.a, (1,:)))
@@ -349,11 +349,11 @@ end
     r = sum(abs.(zonotope.generators .* h'))
     return l <= r
 end=#
-function intersects(zonotope::Zonotope, halfspace::LazySets.HalfSpace) :: Bool
+function intersects(zonotope::Zonotope, halfspace::LazySets.HalfSpace)::Bool
     c::Vector{Float64} = zonotope.center
     G::Matrix{Float64} = genmat(zonotope)
     h::Vector{Float64} = halfspace.a
-    f::Float64 = halfspace.b 
+    f::Float64 = halfspace.b
     #d = c + h
     #d::Vector{Float64} = c.*h
     l::Float64 = abs(f - dot(c, h))#sum(c.* h)
@@ -395,11 +395,11 @@ function linear_map_zonotope_nD(M::AbstractMatrix, c::Vector{Float64}, G::Matrix
     return Zonotope(c, G)
 end
 # https://github.com/JuliaReach/ReachabilityAnalysis.jl/blob/master/src/Discretization/Exponentiation.jl l:341
-function Φ₂(A, δ, invertible = false)
+function Φ₂(A, δ, invertible=false)
     let A = abs.(A)
         if invertible
             Aδ = A .* δ
-            Φ = fastExpm(Aδ; threshold=eps(Float64), nonzero_tol=eps(Float64))#; threshold=eps(Float64), nonzero_tol=eps(Float64))
+            Φ = exp(Aδ) #fastExpm(Aδ; threshold=eps(Float64), nonzero_tol=eps(Float64))#; threshold=eps(Float64), nonzero_tol=eps(Float64))
             n = size(A, 1)
             N = eltype(A)
             In = Matrix(one(N) * I, n, n)
@@ -410,19 +410,19 @@ function Φ₂(A, δ, invertible = false)
         else
             n = LinearAlgebra.checksquare(A)
             C = _P_3n(A, δ, n)
-            P = fastExpm(C; threshold=eps(Float64), nonzero_tol=eps(Float64))
+            P = exp(C) #fastExpm(C; threshold=eps(Float64), nonzero_tol=eps(Float64))
             return _P₂_blk(P, n)
-        end 
+        end
     end
 end
 
 @inline function _P_3n(A::AbstractMatrix{N}, δ, n) where {N}
     return [Matrix(A * δ) Matrix(δ * I, n, n) zeros(n, n);
-            zeros(n, 2 * n) Matrix(δ * I, n, n);
-            zeros(n, 3 * n)]::Matrix{N}
+        zeros(n, 2 * n) Matrix(δ * I, n, n);
+        zeros(n, 3 * n)]::Matrix{N}
 end
 
-@inline _P₂_blk(P, n) = P[1:n, (2 * n + 1):(3 * n)] #@view(P[1:n, (2 * n + 1):(3 * n)])
+@inline _P₂_blk(P, n) = P[1:n, (2*n+1):(3*n)] #@view(P[1:n, (2 * n + 1):(3 * n)])
 
 function E⁺(X, δ, A)
     return convert(Zonotope, symmetric_interval_hull(Φ₂(A, δ, isinvertible(A)) * symmetric_interval_hull(A * A * X)))
