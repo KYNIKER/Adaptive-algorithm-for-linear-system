d2f5ca4bfa7e59166b54d1ca1a706455fe579e80
Daniel Hilo Hansen <55060805+KYNIKER@users.noreply.github.com>
2026-04-26 18:29:13 +0200

    Fix: changed definition of mna1 system in LGG09Test and BFFPSVTest + removed \r from run.sh

--- LGG09Test.jl
+++ LGG09Test.jl
@@ -56,7 +56,8 @@ function doLGG09JuliaTest(load_func, name)
         return
     end
     if _name == "mna1"
-        sys = @system(x' = Ax + B, x ∈ Universe(n))
+        b = @view B[diagind(B)]
+        sys = @system(x' = Ax + b, x ∈ Universe(n))
     else
         sys = @system(x' = Ax + Bu, x ∈ Universe(n), u ∈ ballβ)
     end
